ee57610e87968816d5cd5391d0ea70a400351a57
Meredith Welch <merewelch@gmail.com>
2019-12-03 02:45:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,6 +5,8 @@ def run_guessing_game
   input = gets.chomp
   if input == random
     puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{random}"
   end
