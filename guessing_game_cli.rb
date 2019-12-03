706c760bb077fa44c10af2f3d8fac6bd3c11385c
Meredith Welch <merewelch@gmail.com>
2019-12-03 02:54:39 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
 # Code your solution here!
+def run_guessing_game
+  computer_number = rand(6) + 1
+  input = gets.chomp
+  if input == "exit"
+    puts "Goodbye!"
+  elsif input.to_i == computer_number
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{computer_number}."
+  end
+end
